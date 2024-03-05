0313a7ec73cffb2af3415287c4553919494be3d6
serraguti <pacoserranox@gmail.com>
2024-03-05 14:19:54 +0100

    Add project files.

---
+++ MvcCoreEmpleadosSession/Controllers/EmpleadosController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreEmpleadosSession.Models;
+using MvcCoreEmpleadosSession.Repositories;
+
+namespace MvcCoreEmpleadosSession.Controllers
+{
+    public class EmpleadosController : Controller
+    {
+        private RepositoryEmpleados repo;
+
+        public EmpleadosController(RepositoryEmpleados repo)
+        {
+            this.repo = repo;
+        }
+
+        public async Task<IActionResult> SessionEmpleados()
+        {
+            List<Empleado> empleados =
+                await this.repo.GetEmpleadosAsync();
+            return View(empleados);
+        }
+
+        public IActionResult EmpleadosAlmacenados()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> SessionSalarios(int? salario)
+        {
+            if (salario != null)
+            {
+                //NECESITAMOS ALMACENAR EL SALARIO TOTAL 
+                //DE TODOS LOS EMPLEADOS DE SESSION
+                int sumaSalarial = 0;
+                //PREGUNTAMOS SI YA TENEMOS LA SUMA EN SESSION
+                if (HttpContext.Session.GetString("SUMASALARIAL") != null)
+                {
+                    //RECUPERAMOS LA SUMA SALARIAL DE SESSION
+                    sumaSalarial = int.Parse
+                        (HttpContext.Session.GetString("SUMASALARIAL"));
+                }
+                //REALIZAMOS LA SUMA DEL SALARIO RECIBIDO
+                sumaSalarial += salario.Value;
+                //ALMACENAMOS LA NUEVA SUMA SALARIAL EN SESSION
+                HttpContext.Session.SetString("SUMASALARIAL", sumaSalarial.ToString());
+                ViewData["MENSAJE"] = "Salario almacenado: " + salario.Value;
+            }
+            List<Empleado> empleados = await
+                this.repo.GetEmpleadosAsync();
+            return View(empleados);
+        }
+
+        public IActionResult SumaSalarios()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcCoreEmpleadosSession/Repositories/RepositoryEmpleados.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MvcCoreEmpleadosSession.Data;
+using MvcCoreEmpleadosSession.Models;
+
+namespace MvcCoreEmpleadosSession.Repositories
+{
+    public class RepositoryEmpleados
+    {
+        private EmpleadosContext context;
+
+        public RepositoryEmpleados(EmpleadosContext context)
+        {
+            this.context = context;
+        }
+
+        public async Task<List<Empleado>> GetEmpleadosAsync()
+        {
+            var empleados =
+                await this.context.Empleados.ToListAsync();
+            return empleados;
+        }
+
+        public async Task<Empleado> FindEmpleadoAsync(int idEmpleado)
+        {
+            return await
+                this.context.Empleados.FirstOrDefaultAsync
+                (z => z.IdEmpleado == idEmpleado);
+        }
+    }
+}
